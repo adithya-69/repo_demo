52a4c5a830818fb1469fdbf2dd46e5ae0808b514
adithyasamsung123 <adithyasamsung123@gmail.com>
2025-03-06 12:11:14 +0530

    inital stage

---
+++ calcdemo2.java
@@ -0,0 +1,32 @@
+class calculator
+{
+public static int add(int x,int y)
+{return x+y;}
+public static int sub(int x,int y)
+{return x-y;}
+public static int mul(int x,int y)
+{return x*y;}
+public static int div(int x,int y)
+{return x/y;}
+}
+class ResultPrinter
+{
+public static void display(int value)
+{
+System.out.println("The value is:"+value);
+}
+}
+class calcdemo2
+{
+public static void main(String args[])
+{
+int a=calculator.add(20,30);
+ResultPrinter.display(a);
+int b=calculator.sub(20,30);
+ResultPrinter.display(b);
+int c=calculator.mul(20,30);
+ResultPrinter.display(c);
+int d=calculator.div(20,30);
+ResultPrinter.display(d);
+}
+}
